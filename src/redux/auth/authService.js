076fdb2919eb83b73aacbac4c4c4424a2a81ad81
Sugrish V M <153385658+sug2004@users.noreply.github.com>
2025-08-02 13:44:16 +0530

    fix auth 1

--- src/redux/auth/authService.js
+++ src/redux/auth/authService.js
@@ -1,14 +1,35 @@
+// // src/features/auth/authService.js
+// import axios from 'axios';
+// const API_URL = process.env.REACT_APP_MIPORIS_SERVER;
+
+// const login = async (credentials) => {
+//     const response = await axios.post(`${API_URL}/auth/login`, credentials);
+//     return response.data;
+// };
+
+// const signup = async (userData) => {
+//     const response = await axios.post(`${API_URL}/auth/register`, userData);
+//     return response.data;
+// };
+
+// const authService = { login, signup };
+// export default authService;
+
 // src/features/auth/authService.js
 import axios from 'axios';
 const API_URL = process.env.REACT_APP_MIPORIS_SERVER;
 
 const login = async (credentials) => {
-    const response = await axios.post(`${API_URL}/auth/login`, credentials);
+    const response = await axios.post(`https://app-miporis-eight.vercel.app/api/auth/login`, credentials, {
+        withCredentials: true
+    });
     return response.data;
 };
 
 const signup = async (userData) => {
-    const response = await axios.post(`${API_URL}/auth/register`, userData);
+    const response = await axios.post(`https://app-miporis-eight.vercel.app//api/auth/register`, userData, {
+        withCredentials: true
+    });
     return response.data;
 };
 
